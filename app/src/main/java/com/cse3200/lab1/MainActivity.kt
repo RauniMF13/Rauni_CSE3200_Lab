ea169ea732b9a2cffdc3776c94e434ff77c92b36
RauniMF13 <rauni.machado-filho@uconn.edu>
2023-02-07 22:00:12 -0500

    Lab 1

--- app/src/main/java/com/cse3200/lab1/MainActivity.kt
+++ app/src/main/java/com/cse3200/lab1/MainActivity.kt
@@ -2,10 +2,48 @@ package com.cse3200.lab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.cse3200.lab1.databinding.ActivityMainBinding
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var activityMainBindings: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        activityMainBindings = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBindings.root)
+
+        activityMainBindings.trueButton.setOnClickListener {
+            View.OnClickListener {
+                Toast.makeText(this,"Toast!",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        activityMainBindings.falseButton.setOnClickListener {
+            View.OnClickListener {
+                Toast.makeText(this,"Toast!",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG,"onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
     }
 }
